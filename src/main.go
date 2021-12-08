ccae6d724c899f048c325197995ad1365323bcfb
Password-Classified <85027459+Password-Classified@users.noreply.github.com>
2021-12-08 20:03:53 +0000

    🐛 FIX: Fix error of mine

--- src/main.go
+++ src/main.go
@@ -2,27 +2,29 @@ package main
 
 import (
 	// "time"
+	"time"
+
 	"gopkg.in/toast.v1"
 )
 
 func main() {
-	// for {
-	// time.Sleep(time.Minute * 20)
+	for {
+		time.Sleep(time.Minute * 20)
 
-	notification := toast.Notification{
-		AppID:               "The 20 20 20 Rule",
-		Title:               "Time to Rest Your Eyes",
-		Message:             "It's been 20 minutes, look at something 20 feet away for 20 seconds.",
-		Icon:                "",
-		ActivationType:      "",
-		ActivationArguments: "",
-		Actions:             []toast.Action{
-			{Type: "protocol", Label: "Dismiss", Arguments: ""},
-		},
-		Audio:               "",
-		Loop:                false,
-		Duration:            "",
+		notification := toast.Notification{
+			AppID:               "The 20 20 20 Rule",
+			Title:               "Time to Rest Your Eyes",
+			Message:             "It's been 20 minutes, look at something 20 feet away for 20 seconds.",
+			Icon:                "",
+			ActivationType:      "",
+			ActivationArguments: "",
+			Actions: []toast.Action{
+				{Type: "protocol", Label: "Dismiss", Arguments: ""},
+			},
+			Audio:    "",
+			Loop:     false,
+			Duration: "",
+		}
+		notification.Push()
 	}
-	notification.Push()
-	// }
 }
