3db4185a144dd3bf08d3d900594f2248b942535f
Password-Classified <85027459+Password-Classified@users.noreply.github.com>
2022-02-02 19:14:06 +0000

    👌 IMPROVE: update main.go

--- src/main.go
+++ src/main.go
@@ -1,30 +1,27 @@
 package main
 
 import (
-	// "time"
 	"time"
 
 	"gopkg.in/toast.v1"
 )
 
 func main() {
+	var notification toast.Notification
 	for {
-		time.Sleep(time.Minute * 20)
-
-		notification := toast.Notification{
+		notification = toast.Notification{
 			AppID:               "The 20 20 20 Rule",
 			Title:               "Time to Rest Your Eyes",
 			Message:             "It's been 20 minutes, look at something 20 feet away for 20 seconds.",
 			Icon:                "",
 			ActivationType:      "",
 			ActivationArguments: "",
-			Actions: []toast.Action{
-				{Type: "protocol", Label: "Dismiss", Arguments: ""},
-			},
-			Audio:    "",
-			Loop:     false,
-			Duration: "",
+			Actions:             []toast.Action{{Type: "protocol", Label: "Dismiss", Arguments: ""}},
+			Audio:               "",
+			Loop:                false,
+			Duration:            "long",
 		}
 		notification.Push()
+		time.Sleep(time.Minute * 20)
 	}
 }
